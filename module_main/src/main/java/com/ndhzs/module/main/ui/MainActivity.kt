21de3e7447e050b8e55b582e9ede04498da0b552
Mr-than <64783138+Mr-than@users.noreply.github.com>
2022-06-09 01:06:06 +0800

    写好了main

--- module_main/src/main/java/com/ndhzs/module/main/ui/MainActivity.kt
+++ module_main/src/main/java/com/ndhzs/module/main/ui/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.ndhzs.module.main.ui
 
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.alibaba.android.arouter.launcher.ARouter
@@ -22,19 +27,43 @@ class MainActivity : BaseVmBindActivity<MainActivityViewModel,MainActivityMainBi
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    initContent()
+    liveDataObserve()
 
-/*
+
     lifecycleScope.launch{
       ARouter.getInstance()
         .navigation(IMainService::class.java)
         .dataFlow
         .emit(IMainService.Data("",""))
     }
-*/
+
 
     binding.vpMainActivityPages.adapter=MainVpAdapter(this,listOf(ARouter.getInstance().build(TEST_SHOW).navigation() as Fragment))
+
+  }
+
+  private fun initContent(){
+    if(supportActionBar!=null){
+      setSupportActionBar(binding.mainActivityToolBar)
+    }
+    //将抽屉布局和toolbar关联
+    val toggle = ActionBarDrawerToggle(this,binding.mainActivityDrawerLayoutMainLayout, binding.mainActivityToolBar, 0, 0)
+    toggle.syncState()
+
+    binding.navigationViewMainMore.menu.findItem(R.id.main_nv_menu_integral).run {
+        (actionView as? TextView)?.text=viewModel.getPersonalIntegration()
+    }
 
 
 
   }
+
+
+  private fun liveDataObserve(){
+
+  }
+
+
+
 }
--- module_main/src/main/java/com/ndhzs/module/main/viewmodel/MainActivityViewModel.kt
+++ module_main/src/main/java/com/ndhzs/module/main/viewmodel/MainActivityViewModel.kt
@@ -3,5 +3,14 @@ package com.ndhzs.module.main.viewmodel
 import com.ndhzs.lib.common.ui.BaseViewModel
 
 class MainActivityViewModel:BaseViewModel() {
+    private lateinit var personalIntegration:String
+
+    fun getPersonalIntegration():String{
+        if(!::personalIntegration.isInitialized){
+            personalIntegration="todo"
+        }
+
+        return personalIntegration
+    }
 
 }
